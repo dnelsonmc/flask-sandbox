8d3b6938f514540a934e8d5fac105423e1792f8a
dave <dave@d8-laptop.localhost.localdomain>
2015-05-15 13:55:02 -0400

    added some structure to the app plus templates

--- hello.py
+++ hello.py
@@ -1,21 +1,22 @@
 # hello.py
 import os
-from flask import Flask, request
+from flask import Flask, request, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
+@app.route('/index')
 def index():
-	return '<h1>Hello, Flask World!</h1>'
+	return render_template('index.html')
 
 @app.route('/user/<name>')
 def user(name):
-	return '<h1>Hello, %s!</h1>' % name
+	return render_template('user.html', name=name)
 
 @app.route('/info')
 def info():
 	user_agent = request.headers.get('User-Agent')
-	return '<p>Your browser is %s</p>' % user_agent
+	return render_template('info.html', user_agent=user_agent)
 
 if __name__ == '__main__':
 	app.run(host=os.getenv('IP','0.0.0.0'), port=int(os.getenv('PORT',8080)), debug=True)
